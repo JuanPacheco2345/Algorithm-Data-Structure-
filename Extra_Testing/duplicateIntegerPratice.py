24dccaf8ef364d0ff8533a6a4aed6c320aedbd20
Juan Pacheco <juanpacheco2345@gmail.com>
2024-08-12 23:18:39 -0700

    New Solution

--- Extra_Testing/duplicateIntegerPratice.py
+++ Extra_Testing/duplicateIntegerPratice.py
@@ -177,5 +177,41 @@ def decode(s):
 
 def longestConsecutiveSequence(nums):
     #Sovled
+    pass
 
+
+
+
+def longestSubstringWithoutRepeartingCharacters(s):
+    seen = {}
+    idx = 0
+    longestSubString = 0
+    for i in range(len(s)):
+        while s[i] in seen:
+            seen.remove(s[i])
+            idx += 1
+        seen.add(s[i])
+        longestSubString = max(longestSubString, idx - i + 1)
+
+            
+        
+
+
+
+
+def lengthOfLongestSubstring(s):
+    charSet = set()
+    l = 0
+    res = 0
+
+    for r in range(len(s)):
+        while s[r] in charSet:
+            charSet.remove(s[l])
+            l += 1
+        charSet.add(s[r])
+        res = max(res, r - l + 1)
+    return res
+
+
+lengthOfLongestSubstring("abcabcbb")
 
